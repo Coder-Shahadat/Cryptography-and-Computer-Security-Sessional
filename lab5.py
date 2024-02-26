7afeb161322670f5d97f4fdae9c9f471029e1ee8
MD. Shahadat Hossain Bhuian <1337.Shahadat@gmail.com>
2024-02-26 14:09:06 +0600

    update poly alphabetic

--- lab5.py
+++ lab5.py
@@ -1,4 +1,10 @@
 # Poly_alphabetic cipher
+
+alphabet = "abcdefghijklmnopqrstuvwxyz".upper()
+mp = dict(zip(alphabet, range(len(alphabet))))
+mp2 = dict(zip(range(len(alphabet)), alphabet))
+
+
 def generateKey(plainText, keyword):
     key = ''
     for i in range(len(plainText)):
@@ -9,23 +15,23 @@ def generateKey(plainText, keyword):
 def cipherText(plainText, key):
     cipher_text = ""
     for i in range(len(plainText)):
-        shift = ord(key[i].upper()) - ord('A')
-        new_char = chr((ord(plainText[i].upper()) + shift) % 26 + ord('A'))
-        cipher_text += new_char.lower() if plainText[i].islower() else new_char
+        shift = mp[key[i].upper()] - mp['A']
+        newChar = mp2[(mp[plainText[i].upper()] + shift) % 26]
+        cipher_text += newChar
     return cipher_text
 
 
 def decrypt(cipher_text, key):
     plainText = ''
     for i in range(len(cipher_text)):
-        shift = ord(key[i].upper()) - ord('A')
-        new_char = chr((ord(cipher_text[i].upper()) - shift + 26) % 26 + ord('A'))
-        plainText += new_char.lower() if cipher_text[i].islower() else new_char
+        shift = mp[key[i].upper()] - mp['A']
+        newChar = mp2[(mp[cipher_text[i].upper()] - shift + 26) % 26]
+        plainText += newChar
     return plainText
 
 
-plainText = "Information"
-keyword = "Ice"
+plainText = "wearediscoveredsaveyourself"
+keyword = "deceptive"
 key = generateKey(plainText, keyword)
 cipher_text = cipherText(plainText, key)
 print("Ciphertext :", cipher_text)
